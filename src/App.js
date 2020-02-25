842ab3a7f06b7acc18fdeee7294cb03b1cdbe0f6
Erica Yee <23279506+ericayee@users.noreply.github.com>
2020-02-24 22:15:32 -0500

    bare bones landing page

--- src/App.js
+++ src/App.js
@@ -6,18 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Finding Your Way
+        </h1>
+        <button>
+          View current degree progress
+        </button>
+        <button>
+          Explore degree options
+        </button>
       </header>
     </div>
   );
